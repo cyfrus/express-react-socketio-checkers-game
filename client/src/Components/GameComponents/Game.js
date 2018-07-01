5754c47df4f2e7cd6db8a0548f87efcdfccc65f1
cyfrus <cyfrus1@gmail.com>
2018-07-01 23:40:31 +0200

    game js update

--- client/src/Components/GameComponents/Game.js
+++ client/src/Components/GameComponents/Game.js
@@ -1,108 +1,40 @@
 import React from "react";
 
-
-// class Game extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             board: [],
-//             turn: "red",
-//             pieceLocations: [64],
-//             startingPositions: [1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23, 40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62]
-//         }
-//     }
-//     componentDidMount() {
-//         var board = [];
-//         var cnt = 0;
-//         var piece = false;
-//         for (let index = 0; index < 8; index++) {
-//             var row = [];
-//             board.push(row);
-//             for (let index2 = 0; index2 < 8; index2++) {
-//                 this.state.startingPositions.find(function(element){
-//                     return element === cnt;
-//                 });
-//                 var square = {
-//                     piece: false,
-//                     pieceColor: null,
-//                 }
-//                 board[index].push(square);
-//                 cnt++;
-//             }
-//         }
-//         this.setState({
-//             board: board
-//         })
-//     }
-//     render() {
-//         console.log(this.state.board);
-//         const board = this.state.board.map((row, index) =>
-//         <div className="boardRow" key={index}>
-//         {
-//             row.map(function(square, i){
-//                 return(
-//                     <Square row={index} position={i} key={((index+1) * (i+1)).toString()}/>
-//                 )          
-//             })
-//         }
-//         </div>
-//         );
-//         return(
-//             <div className="row">
-//                 <div className="col-md-5 col-lg-5 boardColumn">
-//                     <div className="board">
-//                         {board}
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     };
-// }
-
-// class Square extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             color: ""
-//         }
-//     }
-//     componentDidMount() {
-//         var color = "";
-//         if(this.props.row % 2 === 0 && this.props.position % 2 === 0) {
-//             color = "white";
-//         } else if ((this.props.row % 2 === 1 && this.props.position % 2 === 0) || (this.props.row % 2 === 0 && this.props.position % 2 === 1)){
-//             color = "black";
-//         } 
-//         this.setState({
-//             color: color
-//         })
-//     }
-//     render() {
-//         return (
-//             <div className={this.state.color + " square"}></div>
-//         );
-//     }
-// }
-
-// class Piece extends React.Component {
-//     constructor(props){
-//         super(props);
-//     }
-//     render() {
-//         return(
-//             <div className="">         
-//             </div>
-//         )
-//     }
-// }
-
 class Game extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            boardState: Array(8).fill(Array(8).fill(null))
+            boardState: [],
+            turn: "red",
+            redPieces: 12,
+            whitePieces: 12
         }
         this.handleClick = this.handleClick.bind(this);
+    }
+    
+    componentDidMount() {
+        var boardState = [8];
+        for(let i = 0; i < 8; i++) {
+                boardState[i] = [];
+           for(let z = 0; z < 8; z++){  
+               if(((i === 0 || i === 2 || i === 6) && (z % 2)) || ((i === 1 || i === 5 || i === 7) && !(z % 2)))
+               {
+                    boardState[i][z] = {piece: true, pieceColor: ""};
+               } else {
+                    boardState[i][z] = {piece: false, pieceColor: ""};
+               }
+               
+           }
+        }
+        // var startingPositions = [[1, 3, 5, 7], [0, 2, 4, 6], [] , [], [], [], [], []];
+        // startingPositions.forEach((row, index) => {
+        //     row.forEach((pieceLocation) => {
+        //         boardState[index][pieceLocation].piece = true;
+        //     })
+        // });
+        this.setState({
+            boardState: boardState
+        })
     }
 
     handleClick(i) {
@@ -121,22 +53,32 @@ class Game extends React.Component {
     }
 }
 
-class Board extends React.Component {  
-    render() {
+class Board extends React.Component {
+    determineColor(row, place) {
+        if((row % 2  && place % 2 === 0) || (row % 2 === 0 && place % 2)) {
+            return "black";
+        } else {
+            return "white";
+        }
+    }
+    renderBoard() {
         var id = 0;
         const board = this.props.squares.map((row, index) => {
-           return <div key={index} className="boardRow"> 
-            {
-                row.map((square) => {
-                    var i = id++;
-                    return <Square  id={i} key={id} className="square" onClick={() => this.props.onClick(i)}/>;
-                })
-            }
+            return <div key={index} className="boardRow">
+                {
+                    row.map((square, place) => {
+                        var i = id++;
+                         return <Square color={this.determineColor(index, place)} id={i} key={id} className="square" piece={square.piece} onClick={() => this.props.onClick(i)} />;
+                    })
+                }
             </div>
         });
+        return board;
+    }
+    render() {
       return (
         <div className="board">
-            {board}
+            {this.renderBoard()}
         </div>
       );
     }
@@ -147,12 +89,27 @@ class Square extends React.Component {
         super(props);
     }
     render() {
-        return(
-            <div className="square" onClick={this.props.onClick}>
-            {this.props.id}
+        if(this.props.piece) {
+           return(
+            <div className={"square " + this.props.color} onClick={this.props.onClick}>
+            <Player />
             </div>
-        );
+        );    
+        } else {
+            return (
+            <div className={"square " + this.props.color} onClick={this.props.onClick}>
+            </div>
+            );
+        }
+        
     }
+}
+
+function Player(props) {
+    return(
+        <div className="player">
+        </div>
+    );
 }
 
 export default Game;
